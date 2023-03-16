85c31209ce2eaac6714395537e1498aff68df785
Fatimaezzahra Matlite <fatimaezzahramatlite@gmail.com>
2023-03-16 13:50:12 -0700

    my commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
+/**
+ * main - A prog that prints a line with puts 
+ *
+ * Return:A Always 0 (success)
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
