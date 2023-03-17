db2956aab4ae66b2e22c69fa2a8637230cb1bfdb
Fatimaezzahra Matlite <fatimaezzahramatlite@gmail.com>
2023-03-17 16:31:42 -0700

    my commit

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Return: 0
+ *
+ */
+int main(void)
+{
+	int n = 0;
+	char c = 'a';
+
+	while (n < 10)
+		putchar(n + '0');
+	while (c < 'g')
+		putchar(a);
+	putchar('\n');
+	return (0);
+}
