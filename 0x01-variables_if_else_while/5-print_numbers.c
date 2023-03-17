bd098574ae9f9a1997c78c8b04c11625c3607482
Fatimaezzahra Matlite <fatimaezzahramatlite@gmail.com>
2023-03-17 16:04:18 -0700

    my commit

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 /**
  * main - main block
- * Return : 0
+ * Return: 0
  */
 int main(void)
 {
 	int i;
 
 	for (i = 0; i < 10; i++)
-		printf("%d\n", i);
+		printf("%d", i);
+	printf("\n");
 	return (0);
 }
