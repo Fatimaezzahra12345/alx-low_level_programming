78a0716114d83aee39c3e238aa58187f74d91b34
Fatimaezzahra Matlite <fatimaezzahramatlite@gmail.com>
2023-03-21 12:47:18 -0700

    my commit

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - function that prints aalphabet
+ *
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+
+	for (i = 0 ; i <= 10 ; i++)
+		print_alphabet();
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -8,6 +8,7 @@
 int printf(const char *, ...);
 void print_alphabet(void);
 int _putchar(char c);
+void print_alphabet_x10(void);
 
 
 
