ec712fa73b964132a124544e2f19cfb63f31081c
Fatimaezzahra Matlite <fatimaezzahramatlite@gmail.com>
2023-03-21 14:59:58 -0700

    my commit

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * jack_bauer()-block
+ */
+void jack_bauer(void)
+{
+	int i, j;
+
+	for (i = 0 ; i < 23 ; i++)
+	{
+		if (i < 10)
+		{
+			_putchar('0' + 0);
+		}
+		_putchar('0' + i);
+		for (j = 0 ; j < 59 ; j++)
+		{
+			if (i < 10)
+			{
+				_putchar('0' + 0);
+			}
+			_putchar('0' + j);
+		}
+	}
