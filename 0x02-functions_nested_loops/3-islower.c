e931055214d6b51071b0ece133e1ba22396a59ad
Fatimaezzahra Matlite <fatimaezzahramatlite@gmail.com>
2023-03-21 13:20:15 -0700

    my commit

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,16 +1,17 @@
 #include "main.h"
 /**
- * _islower - block _islower
- * Return : 1 or 0
+ * _islower()-block _islower
+ * @c: input
+ * Return:1 or 0
  */
 int _islower(int c)
 {
 	char a;
 
-	for (a = "a" ; a <= 'z' ; a++)
+	for (a = 'a' ; a <= 'z' ; a++)
 	{
 		if (c == a)
 			return (1);
 	}
 	return (0);
-
+}
