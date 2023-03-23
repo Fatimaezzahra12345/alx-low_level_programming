c1872ab47bd435dc520c9ae801a16150357c99d7
Fatimaezzahra Matlite <fatimaezzahramatlite@gmail.com>
2023-03-23 10:50:54 -0700

    my commit

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main-block main
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i < 100 ; i++)
+	{
+		if (i % 3 == 0)
+		{
+			printf("Fizz");
+			_putchar(' ');
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz");
+			_putchar(' ');
+		}
+		else if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz");
+			_putchar(' ');
+		}
+		else
+		{
+			printf("%d", i);
+			_putchar(' ');
+		}
+	}
+}
+
