332d44e2f3e4e3f3127c1305d8b6e7bc7391822a
Fatimaezzahra Matlite <fatimaezzahramatlite@gmail.com>
2023-03-27 10:48:38 -0700

    my commit

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * rev_string()-block rev_string()
+ * @s: parameter
+ */
+void rev_string(char *s)
+{
+	int i = 0, j = 0, cpt = 0;
+	char a;
+
+	while (s[i] != '\0')
+	{
+		a[j] = s[i];
+		i++;
+		j++;
+		cpt++;
+	}
+	for (i = 0; j = cpt - 1; i < cpt; j >= 0; i++; j--)
+		s[i] = a[j];
+}
+
