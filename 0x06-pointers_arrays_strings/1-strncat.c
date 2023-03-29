1f5ec12ba2a2bafe0a3621b6b99011c1f8129eda
Fatimaezzahra Matlite <fatimaezzahramatlite@gmail.com>
2023-03-29 11:55:26 -0700

    my commit

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _strncat()-block
+ * @dest: parameter
+ * @src: parameter
+ * @n: parameter
+ * Return: char
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0, k = 0, cpt = 0;
+
+	while (dest[i] != '\0')
+		i++;
+	for (k = 0; cpt < n; k++)
+	{
+		dest[i] = src[k];
+		cpt++;
+		i++;
+	}
+	return (dest);
