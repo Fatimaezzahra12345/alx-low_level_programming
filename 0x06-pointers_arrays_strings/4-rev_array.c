d574ef5c85056bb2b9ffdd87fa60540511d3819d
Fatimaezzahra Matlite <fatimaezzahramatlite@gmail.com>
2023-03-29 17:17:26 -0700

    my commit

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * reverse_array()-reverse_array()
+ * @a: parameter
+ * @n: parameter
+ */
+void reverse_array(int *a, int n)
+{
+	int i, c = n - 1;
+	int *b;
+
+	for (i = 0; i < (n / 2); i++)
+	{
+		b[i] = a[i];
+		a[i] = a[c];
+		a[c] = b[i];
+		c--;
+	}
+}
