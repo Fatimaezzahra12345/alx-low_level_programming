1fc1c5a3ffda0a4b69b2f3dfe4252a6bd2b4b4e8
Fatimaezzahra Matlite <fatimaezzahramatlite@gmail.com>
2023-03-30 09:50:14 -0700

    my commit

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * rot13 - encodes a string into rot13
+ * @s: string to encode
+ * Return: address of s
+ */
+char *rot13(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] >= 'a'  || s[i] <= 'z')
+		{
+			s[i] += 13;
+		}
+		if (s[i] >= 'A'  || s[i] <= 'Z')
+			s[i] += 13;
+	}
+	return (s);
