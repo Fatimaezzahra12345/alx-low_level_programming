25f28ce2c3d2615bcbdedd26948aaa4387410de2
Fatimaezzahra Matlite <fatimaezzahramatlite@gmail.com>
2023-04-03 15:33:39 -0700

    my commit

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strchr()-block _strchr()
+ * @s: a parameter
+ * @c: a parameter
+ * Return: a pointer to the first occurrence of the character
+ */
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	for (i = 0; s[i] !='\0'; i++)
+	{
+		if (s[i] == c)
+			return (s + i);
+	}
+	return (NULL);
+}
