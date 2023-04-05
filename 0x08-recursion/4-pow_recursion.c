6f5d92771cfd51c8b0d0f68ac27e0798e720ac5b
Fatimaezzahra Matlite <fatimaezzahramatlite@gmail.com>
2023-04-05 16:10:06 -0700

    my commit

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -13,6 +13,9 @@ int _pow_recursion(int x, int y)
 		return (-1);
 	else if (y == 0)
 		return (1);
-	i = x * _pow_recursion(x, y--);
-	return (i);
+	else
+	{
+		i = x * _pow_recursion(x, y--);
+		return (i);
+	}
 }
