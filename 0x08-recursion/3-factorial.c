b93b9dbc2be57f15af64da4a261df85fcb92fe55
Fatimaezzahra Matlite <fatimaezzahramatlite@gmail.com>
2023-04-05 11:42:49 -0700

    my commit

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * factorial()-block factorial()
+ * @n: a parameter
+ * Return: int
+ */
+int factorial(int n)
+{
+	int i;
+
+	if (n < 1)
+		return (-1);
+	else if (n == 0)
+		return (1);
+	else
+		i = n * factorial(n - 1);
+	return (i);
+}
