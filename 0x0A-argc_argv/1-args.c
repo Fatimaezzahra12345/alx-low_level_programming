96e9df34b45723039ef4acbea5b191dd12e28936
Fatimaezzahra Matlite <fatimaezzahramatlite@gmail.com>
2023-04-10 11:19:14 -0700

    my commit

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main-block main
+ * @argc: argument count
+ * @argv: arguments
+ * Return: 0
+ */
+int main(int argc, char **argv)
+{
+	printf("%d\n", argc);
+	return (0);
+}
