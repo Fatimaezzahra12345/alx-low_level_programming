05a62f5796f9f6ac967bb56e57c3a3382904a248
Fatimaezzahra Matlite <fatimaezzahramatlite@gmail.com>
2023-04-10 11:09:01 -0700

    my commit

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
- * main - prints its name, followed by a new line.
- * @argc: argument count
- * @argv: arguments
+ * main - prints its name.
+ * @argc: parameter
+ * @argv: parameter
  *
  * Return: 0
  */
