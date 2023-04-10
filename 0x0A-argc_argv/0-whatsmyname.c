be73d43c621fdbf16d083cd39e34eae749e4ab5e
Fatimaezzahra Matlite <fatimaezzahramatlite@gmail.com>
2023-04-10 10:36:55 -0700

    my commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main-block main
+ * @argc: parameter
+ * @argv: parameter
+ * Return: 0
+ */
+int main(int argc, char **argv)
+{
+	printf("¨%s\n", argv[0]);
+	return (0);
+}
