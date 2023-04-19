93f7635097e527f6984acbcfcee6317f7dc0e994
Fatimaezzahra Matlite <fatimaezzahramatlite@gmail.com>
2023-04-19 16:21:07 -0700

    my commit

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * print_name - a function that prints a name.
+ * @name: the name
+ * @f: function 
+ *
+ * Return: void
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
