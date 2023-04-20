d71ab4074d9fcacaf57d5eb590c46c958bcebf85
Fatimaezzahra Matlite <fatimaezzahramatlite@gmail.com>
2023-04-20 12:02:39 -0700

    my commit

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -6,17 +6,15 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
-	va_list list;
+	va_list ab;
 	unsigned int i;
 	int sum = 0;
 	
-	va_start(list, n);
+	va_start(ab, n);
 	if (n == 0)
 		return (0);
 	for (i = 0; i < n; i++)
-		sum += va_arg(list, int);
-	va_end(list);
+		sum += va_arg(ab, int);
+	va_end(ab);
 	return (sum);
 }
-
-
