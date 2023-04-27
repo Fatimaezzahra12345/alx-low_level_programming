868814e85c680f4fdc384fce280ada2cbd226509
Fatimaezzahra Matlite <fatimaezzahramatlite@gmail.com>
2023-04-27 15:27:59 -0700

    my commit

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * print_list() - block print_list()
+ * @h: parameter
+ * Return: number of node
+ */
+size_t print_list(const list_t *h)
+{
+	size_t num = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[%d] %s\n", 0, "(nil)");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		num++;
+		h = h->next;
+	}
+	return (num);
+}
