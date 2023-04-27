85fc0079fccd75b668fe408b6c79b0aee2d2a9d7
Fatimaezzahra Matlite <fatimaezzahramatlite@gmail.com>
2023-04-27 16:54:27 -0700

    my commit

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+
+/**
+ * add_node - add a new node
+ * at the beginning of a list_t list
+ * @head: pointer to the head
+ * @str: string
+ * Return: new element
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new;
+	size_t n;
+
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->str = strdup(str);
+
+	for (n = 0; str[n]; n++)
+		;
+
+	new->len = n;
+	new->next = *head;
+	*head = new;
+
+	return (*head);
+}
