bbc2b23e904be8487898c7ddc143c60ffa3e5214
Fatimaezzahra Matlite <fatimaezzahramatlite@gmail.com>
2023-04-29 10:19:12 -0700

    my commit

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_listint2()-free_listint2()
+ * @head: parameter
+ *
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *new;
+
+	while (*head != NULL)
+	{
+		new = *head;
+		*head = *head->next;
+		free(new);
+	}
+}
+
