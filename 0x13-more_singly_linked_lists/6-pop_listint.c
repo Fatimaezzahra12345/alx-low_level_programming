92939a4c3e127090d78ae2b470ec366a4101d279
Fatimaezzahra Matlite <fatimaezzahramatlite@gmail.com>
2023-04-29 13:38:10 -0700

    my commit

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * pop_listint()-pop_listint() block
+ * @head: parameter
+ *
+ *Return: an int
+ */
+int pop_listint(listint_t **head)
+{
+	int num;
+
+	if (*head == NULL)
+		return (0);
+	num = (*head)->n;
+	free(*head);
+	*head = (*head)->next;
+	return (num);
+}
