2279742862dee9acc15dd9fcc86e688c949d6eb1
Fatimaezzahra Matlite <fatimaezzahramatlite@gmail.com>
2023-04-29 08:41:03 -0700

    my commit

---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * add_nodeint_end()-add_nodeint_end()
+ * @h: a parameter
+ * @n: a aparameter
+ *
+ * Return: listint_t
+ */
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *new, *now;
+
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	now = *head;
+	if (now == NULL)
+		*head = new;
+	else
+	{
+		if (now->next != NULL)
+			now = now->next;
+		now->next = new;
+	}
+	new->next = NULL;
+	return (*head);
+}
+
