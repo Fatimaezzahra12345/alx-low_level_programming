cf5c1e8a79979ebd50c6d95317b756f834a4459f
Fatimaezzahra Matlite <fatimaezzahramatlite@gmail.com>
2023-05-04 16:59:48 -0700

    my commit

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_binary - prints binary representation of a decimal number
+ * @n: decimal number
+ *
+ * Return: void
+ */
+void print_binary(unsigned long int n)
+{
+	unsigned long int tmp;
+
+	aws = n >> 1;
+	if (aws)
+		print_binary(aws);
+	aws <<= 1;
+	if (aws == n)
+		_putchar('0');
+	else
+		_putchar('1');
+}
