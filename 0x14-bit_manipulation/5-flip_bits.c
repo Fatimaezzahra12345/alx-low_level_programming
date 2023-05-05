9d6f3503e4cf2012f33f385cb452236c0b526658
Fatimaezzahra Matlite <fatimaezzahramatlite@gmail.com>
2023-05-04 18:14:35 -0700

    my commit

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * flip_bits - number of different bits between two numbers
+ * @n: first number
+ * @m: second number
+ *
+ * Return : unsigned int
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int d, pow;
+	unsigned int first, a;
+
+	first = 0;
+	pow = 1;
+	d = n ^ m;
+	for (a = 0; a < (sizeof(unsigned long int) * 8); a++)
+	{
+		if (pow == (d & pow))
+			first++;
+		pow <<= 1;
+	}
+	return (first);
+}
