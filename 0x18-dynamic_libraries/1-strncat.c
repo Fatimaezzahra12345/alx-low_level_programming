5fa92b1c339fe5b3c7e47f117f3c44557f599d25
Fatimaezzahra Matlite <fatimaezahramatlite@gmail.com>
2023-06-19 15:17:28 -0700

    my commit

---
+++ 0x18-dynamic_libraries/1-strncat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strncat()-block
+ * @dest: parameter
+ * @src: parameter
+ * @n: parameter
+ * Return: char
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0, k = 0, cpt = 0;
+
+	while (dest[i] != '\0')
+		i++;
+	for (k = 0; src[k] != '\0' && cpt < n; k++)
+	{
+		dest[i] = src[k];
+		cpt++;
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
---
+++ 0x18-dynamic_libraries/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _strchr()-block _strchr()
+ * @s: a parameter
+ * @c: a parameter
+ * Return: a pointer to the first occurrence of the character
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s)
+	{
+		if (*s != c)
+			s++;
+		else
+			return (s);
+	}
+	if (c == '\0')
+		return (s);
+
+	return (NULL);
+}
