3cb6882804fe545ebb304757806de6ffbfad5f29
fatimaezzahra <fatimaezzahramatlite@gmail.com>
2023-09-08 19:48:44 +0000

    my commit

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,25 +1,31 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_get -> Retrieves a value associated with a key
- * @ht: Hash Table
- * @key: Hash Table Key
- * Return: Depend Condition
+ * hash_table_get - gets value associated with a key from hash table
+ * @ht: hash table to get from
+ * @key: key to retrive its value
+ *
+ * Return: value or NULL
  */
 
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-	unsigned long int index = 0;
-	hash_node_t *temp = NULL;
+	unsigned long int index;
+	hash_node_t *temp;
 
-	if (ht == NULL || key == NULL)
-		return (NULL);
+	if ((!key) || (key[0] == '\0') || (!ht))
+		return (0);
+
 	index = key_index((const unsigned char *)key, ht->size);
 	temp = ht->array[index];
-	while (temp != NULL)kk
+	while (temp != NULL)
 	{
-		if (strcmp(temp->key, key) == 0)
+		if (strcmp(key, temp->key) == 0)
+		{
 			return (temp->value);
+		}
 		temp = temp->next;
-	} return (NULL);
+	}
+
+	return (NULL);
 }
